174cec219e2ebebf1f6696462b513b0bc2b34103
nishu-murmu <nishumurmu017@gmail.com>
2023-08-02 15:57:28 +0530

    refact: Login section

--- src/options/sections/login.tsx
+++ src/options/sections/login.tsx
@@ -35,30 +35,42 @@ const Login = () => {
   }, [])
 
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="mx-auto w-1/4 p-12 m-4 rounded-lg bg-[#fafafc] border border-[#e9e9f2]">
-        <div className="text-center text-xl font-semibold">Saved Tokens</div>
-        <div className="flex flex-col">
-          <label htmlFor="editor_token" className="text-md text-gray-500">
-            Editor Token <span className="text-red-600">*</span>
-          </label>
-          <input
-            onChange={(e) => setToken(e.target.value)}
-            type="text"
-            id="editor_token"
-            placeholder="Editor Token"
-            name="Editor Token"
-            className="rounded-md px-4 py-2 text-lg text-black border border-gray-600 focus:border-gray-400"
-          />
-          <button
-            onClick={() => saveToken(token)}
-            className="px-6 py-2 rounded-md text-md text-center bg-[#fed487] text-gray-500 my-4"
-          >
-            Save
-          </button>
+    <section className="bg-gray-50 dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Coupomated Editor Extension
+            </h1>
+            <form className="space-y-4 md:space-y-6" action="#">
+              <div>
+                <label
+                  htmlFor="editor_token"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your Token
+                </label>
+                <input
+                  type="editor_token"
+                  onChange={(e) => setToken(e.target.value)}
+                  name="editor_token"
+                  id="editor_token"
+                  className="bg-gray-50 border border-gray-300 outline-none text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Enter Editor Token"
+                />
+              </div>
+              <button
+                type="button"
+                onClick={() => saveToken(token)}
+                className="rounded-md w-full bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Save Token
+              </button>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   )
 }
 
