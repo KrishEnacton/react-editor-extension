380cf0e984cd01ae4dae0327e50b85c546df4eec
nishu-murmu <nishumurmu017@gmail.com>
2023-08-03 14:36:42 +0530

    fix: styling of the DatePicker

--- src/content/AddCouponSlider/core/DatePicker.tsx
+++ src/content/AddCouponSlider/core/DatePicker.tsx
@@ -3,7 +3,13 @@ import DatePicker from 'react-datepicker'
 
 const CustomDatePicker = () => {
   const [startDate, setStartDate] = useState(new Date())
-  return <DatePicker selected={startDate} onChange={(date) => date != null && setStartDate(date)} />
+  return (
+    <DatePicker
+      className="px-4 py-2 rounded-md w-[345px] border border-slate-700 outline-none"
+      selected={startDate}
+      onChange={(date) => date != null && setStartDate(date)}
+    />
+  )
 }
 
 export default CustomDatePicker
--- src/content/AddCouponSlider/core/InputField.tsx
+++ src/content/AddCouponSlider/core/InputField.tsx
@@ -20,7 +20,7 @@ const InputField: React.FC<{
           type="text"
           id={id}
           onChange={onChange}
-          className="bg-white border border-slate-700 text-slate-700 placeholder-slate-300 text-sm rounded-lg focus:ring-slate-700 focus:border-slate-700 block w-full p-2.5 dark:bg-green-100 dark:border-green-400"
+          className="bg-white border outline-none border-slate-700 text-slate-700 placeholder-slate-300 text-sm rounded-lg focus:ring-slate-700 focus:border-slate-700 block w-full p-2.5 dark:bg-green-100 dark:border-green-400"
           placeholder={placeholder}
         />
       ) : type == 'date' ? (
@@ -29,7 +29,7 @@ const InputField: React.FC<{
         <textarea
           id={id}
           onChange={onChange}
-          className="bg-white border border-slate-700 text-slate-700 placeholder-slate-300 text-sm rounded-lg focus:ring-slate-700 focus:border-slate-700 block w-full p-2.5 dark:bg-green-100 dark:border-green-400"
+          className="bg-white border outline-none border-slate-700 text-slate-700 placeholder-slate-300 text-sm rounded-lg focus:ring-slate-700 focus:border-slate-700 block w-full p-2.5 dark:bg-green-100 dark:border-green-400"
         />
       )}
       {error && <p className="mt-2 text-sm text-red-600">{error}</p>}
